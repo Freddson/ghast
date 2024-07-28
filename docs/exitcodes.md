d04e8f5a71f1db9924f072ff5cc6d1a99f0f4feb
Freddson <mikipolu@gmail.com>
2024-07-28 22:27:51 +0200

    docs: Create docs folder with markdown file for exit code explanations

---
+++ docs/exitcodes.md
@@ -0,0 +1,5 @@
+# Exit codes
+
+Provided below are explanations for various exit statuses found in the code.
+
+``exit status 1`` - Config file not found in provided path.
--- main.go
+++ main.go
@@ -4,7 +4,7 @@ import (
 	"ghast/readflag"
 	"fmt"
 	"os"
-	//"gopkg.in/ini.v1"
+	"gopkg.in/ini.v1"
 )
 
 func main() {
@@ -18,4 +18,17 @@ func main() {
 	if(len(os.Getenv("configPath")) == 0) {
 		fmt.Println("Config path is empty or a directory!")
 	}
+
+	readConf(os.Getenv("configPath"))
+}
+
+func readConf(filepath string) {
+	conf, err := ini.Load(filepath)
+	if (err != nil) {
+		fmt.Printf("Failed to read config file.\n%v", err)
+		fmt.Printf("\n")
+		os.Exit(1)
+	}
+
+	fmt.Println(conf.Section("Connection").Key("AccessToken").String())
 }
